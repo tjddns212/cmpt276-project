55c307e950f0965358ca99c2e86c897c3377c689
seongwoon lee <tjddns212@gmail.com>
2023-06-29 20:59:48 -0700

    created post-page

--- src/main/java/com/example/cmpt276project/controllers/RoomsController.java
+++ src/main/java/com/example/cmpt276project/controllers/RoomsController.java
@@ -1,14 +1,20 @@
 package com.example.cmpt276project.controllers;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.cmpt276project.models.Room;
 import com.example.cmpt276project.models.RoomRepository;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class RoomsController {
@@ -24,4 +30,23 @@ public class RoomsController {
     model.addAttribute("r", rooms);
     return "rooms/showAll";
   }
+
+  @PostMapping("/rooms/add")
+  public String addRoom(@RequestParam Map<String,String> newroom, HttpServletResponse response){
+    System.out.println("ADD room");
+    String newTitle= newroom.get("name");
+    String newAddress=newroom.get("address");
+    String newCity=newroom.get("city");
+    Double newPrice=Double.parseDouble(newroom.get("price"));
+    String newDescription=newroom.get("description");
+    String newStartingDate=newroom.get("startingDate");
+    String newEndingDate=newroom.get("endingDate");
+
+    roomRepo.save(new Room(newTitle,newAddress,newCity,newPrice,newDescription,newStartingDate,newEndingDate));
+    response.setStatus(201);
+
+    return "rooms/addedRoom";
+  }
+
+
 }
