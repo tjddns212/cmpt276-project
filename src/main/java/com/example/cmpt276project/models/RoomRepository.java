f79bb23a22bc95b538e8e1629e3bcc143f2d7eca
luihin903 <luihin903@gmail.com>
2023-07-03 01:59:16 -0700

    Created User Profile

--- src/main/java/com/example/cmpt276project/models/RoomRepository.java
+++ src/main/java/com/example/cmpt276project/models/RoomRepository.java
@@ -1,7 +1,9 @@
 package com.example.cmpt276project.models;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomRepository extends JpaRepository<Room, Integer>{
-  
+    List<Room> findByUid(int uid);
 }
--- src/main/java/com/example/cmpt276project/models/UserRepository.java
+++ src/main/java/com/example/cmpt276project/models/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     List<User> findByEmailAndPassword(String email, String password);
-    
+    List<User> findByUid(int uid);
 }
