138a81be4cc014ab1b014cb966abaa284e00ddb9
luihin903 <luihin903@gmail.com>
2023-07-18 05:50:07 -0700

    Add test for avatar

--- src/test/java/com/example/cmpt276project/Cmpt276ProjectApplicationTests.java
+++ src/test/java/com/example/cmpt276project/Cmpt276ProjectApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.cmpt276project;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.cmpt276project.models.Image;
+import com.example.cmpt276project.models.ImageRepository;
+import com.example.cmpt276project.models.User;
+import com.example.cmpt276project.models.UserRepository;
 
 @SpringBootTest
 class Cmpt276ProjectApplicationTests {
 
+	@Autowired
+	UserRepository userRepo;
+
+	@Autowired
+	ImageRepository imageRepo;
+
+
 	@Test
-	void contextLoads() {
+	void getUserAvatarTest() {
+
+		List<User> users = userRepo.findByUid(8);
+		User user = users.get(0);
+		List<Image> images = imageRepo.findByUid(user.getAvatar());
+		Image image = images.get(0);
+		assertEquals("Screenshot 2023-07-05 224015.png", image.getName());
 	}
 
 }
