1bdc434baf852b98a140480f570fe5e65f85428e
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2023-07-30 13:23:14 -0700

    Write test for deleting a room listing

--- src/test/java/com/example/cmpt276project/DeletePostTest.java
+++ src/test/java/com/example/cmpt276project/DeletePostTest.java
@@ -15,18 +15,19 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
-
+import static org.mockito.Mockito.never;
 import com.example.cmpt276project.models.Image;
 import com.example.cmpt276project.models.Room;
 import com.example.cmpt276project.models.RoomRepository;
 import com.example.cmpt276project.models.User;
-import static org.mockito.Mockito.when;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class OwnerListingsTest {
-
+public class DeletePostTest {
     @Autowired
     private MockMvc mockMvc;
 
@@ -34,28 +35,30 @@ public class OwnerListingsTest {
     private RoomRepository roomRepo;
 
     @Test
-    public void testGetOwnerListings() throws Exception {
+    public void deletePost() throws Exception {
         Integer userId = 1;
-        List<Room> expectedRooms = createExpectedRooms(); // Create a method to generate the expected rooms
+        List<Room> mockedRooms = createMockRooms();
         User mockedUser = new User("John", "Doe", "Jon", "M", "johndoe@gmail.com", "john123", 1, "Landlord");
-        mockedUser.setUid(1);
+        mockedUser.setUid(userId);
+        Room roomToDelete = mockedRooms.get(0);
         // Mock the behavior of the roomService to return the expectedRooms for the
         // specific user ID
         MockHttpSession session = new MockHttpSession();
         session.setAttribute("session_user", mockedUser);
-        when(roomRepo.findAll()).thenReturn(expectedRooms);
-
+        doNothing().when(roomRepo).deleteById(roomToDelete.getUid());
+        /// rooms/delete/{uid}
         // Perform the HTTP GET request to the endpoint with the user ID as a path
         // variable
-        mockMvc.perform(MockMvcRequestBuilders.get("/ownerListings/{id}", userId).session(session))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("rooms/ownerListings"))
-                .andExpect(MockMvcResultMatchers.model().attributeExists("listings"))
-                .andExpect(MockMvcResultMatchers.model().attribute("listings", expectedRooms));
+        String endpointID = "/rooms/delete/" + roomToDelete.getUid();
+        mockMvc.perform(MockMvcRequestBuilders.post(endpointID).session(session))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection());
+        verify(roomRepo).deleteById(roomToDelete.getUid());
+
+        verify(roomRepo, never()).deleteById(mockedRooms.get(1).getUid());
+
     }
 
-    // Helper method to create a list of expected rooms for testing
-    private List<Room> createExpectedRooms() {
+    private List<Room> createMockRooms() {
         List<Room> expectedRooms = new ArrayList<>();
         Room room1 = new Room("TestingRoom", "TestingAddress", "Vancouver", 50.5, "Testing house", "2023/09/11",
                 "2024/09/11", 1);
@@ -65,6 +68,14 @@ public class OwnerListingsTest {
         Image mockImage = new Image("example_image", "jpg", imageData);
         room1.setImage(mockImage);
         expectedRooms.add(room1);
+        Room room2 = new Room("TestingRoom2", "TestingAddress2", "Vancouver", 50.5, "Testing house", "2023/09/11",
+                "2024/09/11", 2);
+
+        // Instantiate a MockImage with sample data
+        Image mockImage2 = new Image("example_image", "jpg", imageData);
+        room2.setImage(mockImage2);
+        room2.setUid(2);
+        expectedRooms.add(room2);
         return expectedRooms;
     }
 }
