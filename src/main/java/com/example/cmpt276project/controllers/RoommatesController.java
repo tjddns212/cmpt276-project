a7824ee30ab3d48cb200c5e5215a1d41d9d1a8f4
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2023-07-30 17:14:17 -0700

    Add rendering functionality to display potential roommates using info cards

--- src/main/java/com/example/cmpt276project/controllers/RoommatesController.java
+++ src/main/java/com/example/cmpt276project/controllers/RoommatesController.java
@@ -1,5 +1,8 @@
 package com.example.cmpt276project.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,8 +24,15 @@ public class RoommatesController {
     @GetMapping("/findRoomates")
     public String showPossibleRoommates(Model model, HttpSession session) {
         User user = (User) session.getAttribute("session_user");
+        if (user == null) {
+            return "/login";
+        }
         model.addAttribute("user", user);
-
+        List<User> possibleRoommates = userRepo.findAll().stream()
+                .filter(possibleRoommate -> possibleRoommate.getUid() != user.getUid()
+                        && possibleRoommate.getAccountType().equals("Student"))
+                .collect(Collectors.toList());
+        model.addAttribute("roommates", possibleRoommates);
         return "roommates/RoommatesSearch";
     }
 }
