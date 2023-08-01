867744d57617b84d2cb1d40e30557a8d0c05226a
luihin903 <luihin903@gmail.com>
2023-08-01 13:41:09 -0700

    Add avatars to posts in community/forum

--- src/main/java/com/example/cmpt276project/controllers/CommunityController.java
+++ src/main/java/com/example/cmpt276project/controllers/CommunityController.java
@@ -2,7 +2,10 @@ package com.example.cmpt276project.controllers;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Base64;
+import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +15,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.cmpt276project.models.Image;
+import com.example.cmpt276project.models.ImageRepository;
 import com.example.cmpt276project.models.Post;
 import com.example.cmpt276project.models.PostRepository;
 import com.example.cmpt276project.models.User;
+import com.example.cmpt276project.models.UserRepository;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -24,10 +30,37 @@ public class CommunityController {
     @Autowired
     PostRepository postRepo;
 
+    @Autowired
+    ImageRepository imageRepo;
+
+    @Autowired
+    UserRepository userRepo;
+
     @GetMapping("Community")
     public String getPosts(Model model, HttpSession session) {
         List<Post> posts = postRepo.findAll();
+        Collections.reverse(posts);
         model.addAttribute("posts", posts);
+
+        List<String> imageDatas = new ArrayList<String>();
+        List<String> imageTypes = new ArrayList<String>();
+
+        for (int i = 0; i < posts.size(); i ++) {
+            List<Image> images = imageRepo.findByUid(userRepo.findByUid(posts.get(i).getPoster()).get(0).getAvatar());
+            Image image = images.get(0);
+            byte[] imageData = image.getImage();
+            String base64Image = Base64.getEncoder().encodeToString(imageData);
+
+            imageDatas.add(base64Image);
+            imageTypes.add(image.getType());
+        }
+
+        model.addAttribute("imageDatas", imageDatas);
+        model.addAttribute("imageTypes", imageTypes);
+        
+        List<Integer> length = nums(posts.size());
+        model.addAttribute("length", length);
+        
         return "forum";
     }
 
@@ -54,5 +87,13 @@ public class CommunityController {
         String fdt = dt.format(f);
 
         return fdt;
+    }
+
+    public List<Integer> nums(int n) {
+        List<Integer> li = new ArrayList<Integer>();
+        for (int i = 0; i < n; i ++) {
+            li.add(i);
+        }
+        return li;
     }
 }
