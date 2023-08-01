bb969db8916d919d9722907ecae98b76df0f642b
clc31 <clc31@sfu.ca>
2023-08-01 11:23:16 -0700

    Add selenium student signup, login and logout test

--- src/test/java/com/example/cmpt276project/SeleniumIntegrationTests.java
+++ src/test/java/com/example/cmpt276project/SeleniumIntegrationTests.java
@@ -26,12 +26,19 @@ public class SeleniumIntegrationTests {
 
     private String domainUri = "https://roomlink.onrender.com";
 
-    private String firstName = "Cheuk Lam";
-    private String lastName = "Chan";
-    private String nickname = "Jan";
-    private String gender = "F";
-    private String email = "123456@123456.com";
-    private String password = "123456";
+    private String landlordFirstName = "Landlord First Name";
+    private String landlordLastName = "Landlord Last Name";
+    private String landlordNickname = "Landlord Nickname";
+    private String landlordGender = "M";
+    private String landlordEmail = "landlord@123456.com";
+    private String landlordPassword = "123456";
+
+    private String studentFirstName = "Student First Name";
+    private String studentLastName = "Student Last Name";
+    private String studentNickname = "Student Nickname";
+    private String studentGender = "F";
+    private String studentEmail = "student@123456.com";
+    private String studentPassword = "123456";
 
     int waitTime = 1000;
 
@@ -47,8 +54,15 @@ public class SeleniumIntegrationTests {
 
     @Test
     @Order(1)
-    public void deleteTestUser() {
-        List<User> users = userRepository.findByEmail(email);
+    public void deleteTestUsers() {
+        List<User> users;
+
+        users = userRepository.findByEmail(landlordEmail);
+        if (!users.isEmpty()) {
+            userRepository.deleteAll(users);
+        }
+
+        users = userRepository.findByEmail(studentEmail);
         if (!users.isEmpty()) {
             userRepository.deleteAll(users);
         }
@@ -80,22 +94,22 @@ public class SeleniumIntegrationTests {
         currentUri = domainUri + "/adduserLandlord.html";
         assert(currentUri.equals(driver.getCurrentUrl()));
         WebElement firstNameEdittext = driver.findElement(By.xpath("//*[@id=\"first\"]"));
-        firstNameEdittext.sendKeys(firstName);
+        firstNameEdittext.sendKeys(landlordFirstName);
         WebElement lastNameEdittext = driver.findElement(By.xpath("//*[@id=\"last\"]"));
-        lastNameEdittext.sendKeys(lastName);
+        lastNameEdittext.sendKeys(landlordLastName);
         WebElement nicknameEdittext = driver.findElement(By.xpath("//*[@id=\"nick\"]"));
-        nicknameEdittext.sendKeys(nickname);
+        nicknameEdittext.sendKeys(landlordNickname);
         WebElement genderEdittext = driver.findElement(By.xpath("//*[@id=\"gender\"]"));
-        genderEdittext.sendKeys(gender);
+        genderEdittext.sendKeys(landlordGender);
         WebElement emailEdittext = driver.findElement(By.xpath("//*[@id=\"email\"]"));
-        emailEdittext.sendKeys(email);
+        emailEdittext.sendKeys(landlordEmail);
         WebElement passwordEdittext = driver.findElement(By.xpath("//*[@id=\"password\"]"));
-        passwordEdittext.sendKeys(password);
+        passwordEdittext.sendKeys(landlordPassword);
         WebElement createButton = driver.findElement(By.xpath("/html/body/main/form/div[7]/input"));
         createButton.submit();
         Thread.sleep(waitTime);
 
-        // Add user landlord page
+        // User landlord page
         currentUri = domainUri + "/user/adduserLandlord";
         assert(currentUri.equals(driver.getCurrentUrl()));
         WebElement messageLabel = driver.findElement(By.xpath("/html/body/p"));
@@ -129,22 +143,22 @@ public class SeleniumIntegrationTests {
         currentUri = domainUri + "/adduserLandlord.html";
         assert(currentUri.equals(driver.getCurrentUrl()));
         WebElement firstNameEdittext = driver.findElement(By.xpath("//*[@id=\"first\"]"));
-        firstNameEdittext.sendKeys(firstName);
+        firstNameEdittext.sendKeys(landlordFirstName);
         WebElement lastNameEdittext = driver.findElement(By.xpath("//*[@id=\"last\"]"));
-        lastNameEdittext.sendKeys(lastName);
+        lastNameEdittext.sendKeys(landlordLastName);
         WebElement nicknameEdittext = driver.findElement(By.xpath("//*[@id=\"nick\"]"));
-        nicknameEdittext.sendKeys(nickname);
+        nicknameEdittext.sendKeys(landlordNickname);
         WebElement genderEdittext = driver.findElement(By.xpath("//*[@id=\"gender\"]"));
-        genderEdittext.sendKeys(gender);
+        genderEdittext.sendKeys(landlordGender);
         WebElement emailEdittext = driver.findElement(By.xpath("//*[@id=\"email\"]"));
-        emailEdittext.sendKeys(email);
+        emailEdittext.sendKeys(landlordEmail);
         WebElement passwordEdittext = driver.findElement(By.xpath("//*[@id=\"password\"]"));
-        passwordEdittext.sendKeys(password);
+        passwordEdittext.sendKeys(landlordPassword);
         WebElement createButton = driver.findElement(By.xpath("/html/body/main/form/div[7]/input"));
         createButton.submit();
         Thread.sleep(waitTime);
 
-        // Add user landlord page
+        // User landlord page
         currentUri = domainUri + "/user/adduserLandlord";
         assert(currentUri.equals(driver.getCurrentUrl()));
         WebElement messageLabel = driver.findElement(By.xpath("/html/body/p"));
@@ -170,9 +184,9 @@ public class SeleniumIntegrationTests {
         currentUri = domainUri + "/login";
         assert(currentUri.equals(driver.getCurrentUrl()));
         WebElement emailEdittext = driver.findElement(By.xpath("//*[@id=\"email\"]"));
-        emailEdittext.sendKeys(email);
+        emailEdittext.sendKeys(landlordEmail);
         WebElement passwordEdittext = driver.findElement(By.xpath("//*[@id=\"password\"]"));
-        passwordEdittext.sendKeys(password);
+        passwordEdittext.sendKeys(landlordPassword);
         WebElement loginButton2 = driver.findElement(By.xpath("/html/body/form/fieldset/a/input"));
         loginButton2.submit();
         Thread.sleep(waitTime);
@@ -190,5 +204,159 @@ public class SeleniumIntegrationTests {
         currentUri = domainUri + "/";
         assert(currentUri.equals(driver.getCurrentUrl()));
         Thread.sleep(waitTime);
+    }
+
+    @Test
+    @Order(5)
+    public void signupAddStudentAccountSucceeded() throws InterruptedException {
+        String currentUri = domainUri + "/";
+        driver.get(currentUri);
+        Thread.sleep(waitTime);
+
+        // Main page
+        assert(currentUri.equals(driver.getCurrentUrl()));
+        WebElement signupButton = driver.findElement(By.xpath("//*[@id=\"sign-up\"]"));
+        WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"log-in\"]"));
+        signupButton.click();
+        Thread.sleep(waitTime);
+
+        // Choose user page
+        currentUri = domainUri + "/chooseuser.html";
+        assert(currentUri.equals(driver.getCurrentUrl()));
+        WebElement landlordButton = driver.findElement(By.xpath("/html/body/div[3]/figure[1]/a/img"));
+        WebElement studentButton = driver.findElement(By.xpath("/html/body/div[3]/figure[2]/a/img"));
+        studentButton.click();
+        Thread.sleep(waitTime);
+
+        // Add user student page
+        currentUri = domainUri + "/adduser.html";
+        assert(currentUri.equals(driver.getCurrentUrl()));
+        WebElement firstNameEdittext = driver.findElement(By.xpath("//*[@id=\"first\"]"));
+        firstNameEdittext.sendKeys(studentFirstName);
+        WebElement lastNameEdittext = driver.findElement(By.xpath("//*[@id=\"last\"]"));
+        lastNameEdittext.sendKeys(studentLastName);
+        WebElement nicknameEdittext = driver.findElement(By.xpath("//*[@id=\"nick\"]"));
+        nicknameEdittext.sendKeys(studentNickname);
+        WebElement genderEdittext = driver.findElement(By.xpath("//*[@id=\"gender\"]"));
+        genderEdittext.sendKeys(studentGender);
+        WebElement emailEdittext = driver.findElement(By.xpath("//*[@id=\"email\"]"));
+        emailEdittext.sendKeys(studentEmail);
+        WebElement passwordEdittext = driver.findElement(By.xpath("//*[@id=\"password\"]"));
+        passwordEdittext.sendKeys(studentPassword);
+        WebElement createButton = driver.findElement(By.xpath("/html/body/main/form/div[7]/input"));
+        createButton.submit();
+        Thread.sleep(waitTime);
+
+        // User student page
+        currentUri = domainUri + "/user/adduser";
+        assert(currentUri.equals(driver.getCurrentUrl()));
+        WebElement messageLabel = driver.findElement(By.xpath("/html/body/p"));
+        assert("New Record Successfully Created".equals(messageLabel.getText()));
+        Thread.sleep(waitTime);
+    }
+
+    @Test
+    @Order(6)
+    public void signupAddStudentAccountFailed() throws InterruptedException {
+        String currentUri = domainUri + "/";
+        driver.get(currentUri);
+        Thread.sleep(waitTime);
+
+        // Main page
+        assert(currentUri.equals(driver.getCurrentUrl()));
+        WebElement signupButton = driver.findElement(By.xpath("//*[@id=\"sign-up\"]"));
+        WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"log-in\"]"));
+        signupButton.click();
+        Thread.sleep(waitTime);
+
+        // Choose user page
+        currentUri = domainUri + "/chooseuser.html";
+        assert(currentUri.equals(driver.getCurrentUrl()));
+        WebElement landlordButton = driver.findElement(By.xpath("/html/body/div[3]/figure[1]/a/img"));
+        WebElement studentButton = driver.findElement(By.xpath("/html/body/div[3]/figure[2]/a/img"));
+        studentButton.click();
+        Thread.sleep(waitTime);
+
+        // Add user student page
+        currentUri = domainUri + "/adduser.html";
+        assert(currentUri.equals(driver.getCurrentUrl()));
+        WebElement firstNameEdittext = driver.findElement(By.xpath("//*[@id=\"first\"]"));
+        firstNameEdittext.sendKeys(studentFirstName);
+        WebElement lastNameEdittext = driver.findElement(By.xpath("//*[@id=\"last\"]"));
+        lastNameEdittext.sendKeys(studentLastName);
+        WebElement nicknameEdittext = driver.findElement(By.xpath("//*[@id=\"nick\"]"));
+        nicknameEdittext.sendKeys(studentNickname);
+        WebElement genderEdittext = driver.findElement(By.xpath("//*[@id=\"gender\"]"));
+        genderEdittext.sendKeys(studentGender);
+        WebElement emailEdittext = driver.findElement(By.xpath("//*[@id=\"email\"]"));
+        emailEdittext.sendKeys(studentEmail);
+        WebElement passwordEdittext = driver.findElement(By.xpath("//*[@id=\"password\"]"));
+        passwordEdittext.sendKeys(studentPassword);
+        WebElement createButton = driver.findElement(By.xpath("/html/body/main/form/div[7]/input"));
+        createButton.submit();
+        Thread.sleep(waitTime);
+
+        // User student page
+        currentUri = domainUri + "/user/adduser";
+        assert(currentUri.equals(driver.getCurrentUrl()));
+        WebElement messageLabel = driver.findElement(By.xpath("/html/body/p"));
+        assert("This email is used".equals(messageLabel.getText()));
+        Thread.sleep(waitTime);
+    }
+
+    @Test
+    @Order(7)
+    public void loginAndLogoutStudentAccount() throws InterruptedException {
+        String currentUri = domainUri + "/";
+        driver.get(currentUri);
+        Thread.sleep(waitTime);
+
+        // Main page
+        assert(currentUri.equals(driver.getCurrentUrl()));
+        WebElement signupButton = driver.findElement(By.xpath("//*[@id=\"sign-up\"]"));
+        WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"log-in\"]"));
+        loginButton.click();
+        Thread.sleep(waitTime);
+
+        // Login page
+        currentUri = domainUri + "/login";
+        assert(currentUri.equals(driver.getCurrentUrl()));
+        WebElement emailEdittext = driver.findElement(By.xpath("//*[@id=\"email\"]"));
+        emailEdittext.sendKeys(studentEmail);
+        WebElement passwordEdittext = driver.findElement(By.xpath("//*[@id=\"password\"]"));
+        passwordEdittext.sendKeys(studentPassword);
+        WebElement loginButton2 = driver.findElement(By.xpath("/html/body/form/fieldset/a/input"));
+        loginButton2.submit();
+        Thread.sleep(waitTime);
+
+        // Main page
+        currentUri = domainUri + "/";
+        assert(currentUri.equals(driver.getCurrentUrl()));
+        WebElement profileButton = driver.findElement(By.xpath("//*[@id=\"nav\"]/div/div[1]/img"));
+        profileButton.click();
+        WebElement logoutButton = driver.findElement(By.xpath("//*[@id=\"nav\"]/div/div[2]/ul/li[3]/a"));
+        logoutButton.click();
+        Thread.sleep(waitTime);
+
+        // Main page
+        currentUri = domainUri + "/";
+        assert(currentUri.equals(driver.getCurrentUrl()));
+        Thread.sleep(waitTime);
+    }
+
+    @Test
+    @Order(100)
+    public void deleteTestUsersAgain() {
+        List<User> users;
+
+        users = userRepository.findByEmail(landlordEmail);
+        if (!users.isEmpty()) {
+            userRepository.deleteAll(users);
+        }
+
+        users = userRepository.findByEmail(studentEmail);
+        if (!users.isEmpty()) {
+            userRepository.deleteAll(users);
+        }
     }
 }
