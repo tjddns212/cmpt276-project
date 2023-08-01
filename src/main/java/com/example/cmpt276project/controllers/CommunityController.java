18dbb0902ca41c74031b236936f94a3c932b1b18
luihin903 <luihin903@gmail.com>
2023-08-01 02:07:45 -0700

    Able to upload and view posts.
    Haven't embellish at all.

--- src/main/java/com/example/cmpt276project/controllers/CommunityController.java
+++ src/main/java/com/example/cmpt276project/controllers/CommunityController.java
@@ -1,15 +1,20 @@
 package com.example.cmpt276project.controllers;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.cmpt276project.models.Post;
 import com.example.cmpt276project.models.PostRepository;
+import com.example.cmpt276project.models.User;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -27,7 +32,27 @@ public class CommunityController {
     }
 
     @PostMapping("Posting")
-    public String newPost(HttpSession session) {
-        return "posting";
+    public String newPost(@RequestParam Map<String, String> post, HttpSession session) {
+        User poster = (User) session.getAttribute("session_user");
+        
+        Post p = new Post();
+
+        p.setTopic(post.get("title"));
+        p.setContent(post.get("content"));
+        p.setNick(poster.getNick());
+        p.setPoster(poster.getUid());
+        p.setTime(getTime());
+        
+        postRepo.save(p);
+        
+        return "Success";
+    }
+
+    public String getTime() {
+        LocalDateTime dt = LocalDateTime.now();
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String fdt = dt.format(f);
+
+        return fdt;
     }
 }
