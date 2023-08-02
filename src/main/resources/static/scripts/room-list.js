5700c58cecd9b6859a05350659e3070c38218e30
seongwoon lee <tjddns212@gmail.com>
2023-08-01 21:15:01 -0700

    now input on index.html is working

--- src/main/resources/static/scripts/room-list.js
+++ src/main/resources/static/scripts/room-list.js
@@ -2,6 +2,15 @@ const priceFilterButton = document.getElementById("price-filter");
 const locationSelect = document.querySelector("select");
 const roomCards = document.querySelectorAll(".room-card");
 
+window.onload = function () {
+  let urlParams = new URLSearchParams(window.location.search);
+  let locationFilter = urlParams.get("location");
+  if (locationFilter) {
+    locationSelect.value = decodeURIComponent(locationFilter);
+    sortByLocation();
+  }
+};
+
 priceFilterButton.addEventListener("click", () => {
   sortByPrice();
 });
@@ -30,11 +39,38 @@ function sortByPrice() {
 
 function sortByLocation() {
   const selectedLocation = locationSelect.value;
-  roomCards.forEach((card) => {
-    const city = card.querySelector("#city").textContent;
-    card.style.display = city === selectedLocation ? "block" : "none";
-  });
+
+  // Array of valid locations
+  const validLocations = [
+    "vancouver",
+    "surrey",
+    "richmond",
+    "burnaby",
+    "coquitlam",
+    "delta",
+    "langley",
+    "northVancouver",
+    "mapleRidge",
+    "newWestminster",
+    "portCoquitlam",
+    "westVancouver",
+    "whiteRock",
+    "pittMeadows",
+  ];
+
+  // Show all cards if the selected location is not in the list of valid locations
+  if (!validLocations.includes(selectedLocation)) {
+    roomCards.forEach((card) => {
+      card.style.display = "block";
+    });
+  } else {
+    roomCards.forEach((card) => {
+      const city = card.querySelector("#city").textContent;
+      card.style.display = city === selectedLocation ? "block" : "none";
+    });
+  }
 }
+
 function menuToggle() {
   //user dropdown menu
   const toggleMenu = document.querySelector(".menu");
